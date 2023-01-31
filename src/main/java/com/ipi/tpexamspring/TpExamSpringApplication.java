fcdde46828ece0696607fa5cda0d6b944bf47415
kevin.toukam <kevin.toukam@office.aubay.it>
2023-01-31 12:42:00 +0100

    this is my first commit

---
+++ src/main/java/com/ipi/tpexamspring/TpExamSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ipi.tpexamspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpExamSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpExamSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ipi/tpexamspring/TpExamSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipi.tpexamspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpExamSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
